548a7ed29696297b774c2a97a23fc9e7ee6d3e1c
sumanpatra03 <sumanpatra32003@gmail.com>
2025-04-06 11:56:19 +0530

    update

--- app/api/invite-user/route.ts
+++ app/api/invite-user/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_AcoVh9cJ_JjGkjgxkpGoD484sWVgangnz");
 
 export async function POST(request: Request) {
   try {
